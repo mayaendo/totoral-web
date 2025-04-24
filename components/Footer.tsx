f3747b8a1b2e3c6fb243e436b9c6bc4d93e4492e
simonsousa <simonsousavelarde@gmail.com>
2025-04-24 13:34:14 -0500

    galeria del estudio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import Image from "next/image";
 
 export default function Footer() {
   return (
@@ -7,11 +8,14 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-6 py-8 grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
         {/* Logo y descripción a la izquierda */}
         <div className="flex flex-col items-start">
-          <img
-            src="/images/Logo2.jpg"
+          <Image
+            src="/images/Logo2.jpg" // o .png o el formato que estés usando
             alt="El Totoral Logo"
-            className="mb-2 w-28"
+            width={128}
+            height={40}
+            className="mb-4 w-32 h-auto"
           />
+
           <p className="text-gray-300 leading-tight">
             El Totoral <br />— El estudio donde la musica echa raices
           </p>
@@ -21,11 +25,21 @@ export default function Footer() {
         <div>
           <h3 className="font-bold mb-2">Navegación</h3>
           <div className="grid grid-cols-2 gap-x-1 gap-y-0.5">
-            <Link href="/" className="hover:text-[#ffee8d]">Inicio</Link>
-            <Link href="/trabajo" className="hover:text-[#ffee8d]">Trabajo</Link>
-            <Link href="/contacto" className="hover:text-[#ffee8d]">Contacto</Link>
-            <Link href="/estudio" className="hover:text-[#ffee8d]">El Estudio</Link>
-            <Link href="/servicios" className="hover:text-[#ffee8d]">Servicios</Link>
+            <Link href="/" className="hover:text-[#ffee8d]">
+              Inicio
+            </Link>
+            <Link href="/trabajo" className="hover:text-[#ffee8d]">
+              Trabajo
+            </Link>
+            <Link href="/contacto" className="hover:text-[#ffee8d]">
+              Contacto
+            </Link>
+            <Link href="/estudio" className="hover:text-[#ffee8d]">
+              El Estudio
+            </Link>
+            <Link href="/servicios" className="hover:text-[#ffee8d]">
+              Servicios
+            </Link>
           </div>
         </div>
 
@@ -62,7 +76,8 @@ export default function Footer() {
 
       {/* Derechos reservados dentro del bloque */}
       <div className="text-center text-gray-500 border-t border-gray-700 pt-4 pb-6">
-        &copy; {new Date().getFullYear()} El Totoral. Todos los derechos reservados.
+        &copy; {new Date().getFullYear()} El Totoral. Todos los derechos
+        reservados.
       </div>
     </footer>
   );
